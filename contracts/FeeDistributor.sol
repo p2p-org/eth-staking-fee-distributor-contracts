566d50b22841031129a2892b4e5315113d707aeb
Alexander Biryukov <alexander.biryukov@p2p.org>
2022-10-04 15:38:12 +0300

    add comments to contracts

--- contracts/FeeDistributor.sol
+++ contracts/FeeDistributor.sol
@@ -15,12 +15,23 @@ contract FeeDistributor {
     using Address for address payable;
 
     // State variables
+
+    /// address of FeeDistributorFactory
     address private immutable i_factory;
+
+    /// address of the owner
     address private immutable i_owner;
+
+    /// address of the service (P2P) fee recipient
     address payable private immutable i_service;
+
+    /// % of EL rewards that should go to the service (P2P)
     uint256 private immutable i_servicePercent;
+
+    /// % of EL rewards that should go to the client
     uint256 private immutable i_clientPercent;
 
+    /// address of the client
     address payable private s_client;
 
     // Events
@@ -36,16 +47,8 @@ contract FeeDistributor {
         _;
     }
 
-    // Functions Order:
-    //// constructor
-    //// receive
-    //// fallback
-    //// external
-    //// public
-    //// internal
-    //// private
-    //// view / pure
-
+    /** @dev Set values that are constant, common for all the clients, known at the initial deploy time.
+    */
     constructor(
         address _factory,
         address _owner,
@@ -64,16 +67,30 @@ contract FeeDistributor {
         i_clientPercent = _clientPercent;
     }
 
+    // Functions
+
+    /** @dev Set client address. Could not be in the constructor since it is different for different clients.
+    */
     function initialize(address _client) external onlyFactory {
         s_client = payable(_client);
     }
 
-    function withdraw() external onlyOwner {
+    /** @dev Withdraw the whole balance of the contract according to the pre-defined percentages.
+    */
+    function withdraw() external {
+        // get the contract's balance
         uint256 balance = address(this).balance;
+
+        // how much should service get
         uint256 serviceAmount = balance * i_servicePercent / 100;
+
+        // how much should client get
         uint256 clientAmount = balance * i_clientPercent / 100;
 
+        // send ETH to service
         i_service.sendValue(serviceAmount);
+
+        // send ETH to client
         s_client.sendValue(clientAmount);
     }
 }
--- contracts/FeeDistributorFactory.sol
+++ contracts/FeeDistributorFactory.sol
@@ -13,19 +13,40 @@ contract FeeDistributorFactory is Ownable {
     using Clones for address;
 
     // State variables
+
+    /** @dev The address of the reference implementation of FeeDistributor
+    * that is used as the basis for clones
+    */
     address private s_referenceFeeDistributor;
 
     // Events
+
+    /** @dev Emits when a new FeeDistributor instance has been created for a client
+    */
     event FeeDistributorCreated(address newFeeDistributorAddrress);
 
+    // Functions
+
+    /** @dev Set a new reference implementation of FeeDistributor
+    */
     function initialize(address _referenceFeeDistributor) external onlyOwner {
         s_referenceFeeDistributor = _referenceFeeDistributor;
     }
 
+    /** @dev Creates a FeeDistributor instance for a client
+    * Emits `FeeDistributorCreated` event with the address of the newly created instance
+    */
     function createFeeDistributor(address _client) external onlyOwner {
+        // clone the reference implementation of FeeDistributor
         address newFeeDistributorAddrress = s_referenceFeeDistributor.clone();
+
+        // cast address to FeeDistributor
         FeeDistributor newFeeDistributor = FeeDistributor(newFeeDistributorAddrress);
+
+        // set the client address to the cloned FeeDistributor instance
         newFeeDistributor.initialize(_client);
+
+        // emit event with the address of the newly created instance for the external listener
         emit FeeDistributorCreated(newFeeDistributorAddrress);
     }
 }

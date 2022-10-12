79e464cf1ddf700fe753069a4aec9bc027c5ec70
Alexander Biryukov <alexander.biryukov@p2p.org>
2022-10-12 14:34:10 +0300

    address review https://hackmd.io/888oWWLeQ7C9bdX_oGHq7Q?view for Reference commit: 178d5aa4fac1bf1a0570afc4bd61dca46ebc0aaa

--- contracts/FeeDistributor.sol
+++ contracts/FeeDistributor.sol
@@ -1,86 +1,170 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity 0.8.17;
+
 import "@openzeppelin/contracts/utils/Address.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/introspection/ERC165Checker.sol";
+import "./IFeeDistributorFactory.sol";
 
-error FeeDistributor__NotOwner();
-error FeeDistributor__NotFactory();
-error PercentsDoNotMatch();
-
-/** @title Contract receiving MEV and priority fees
-* and distibuting them to the service and the client
+/**
+* @notice Should be a FeeDistributorFactory contract
+* @param _passedAddress passed address that does not support IFeeDistributorFactory interface
 */
-contract FeeDistributor {
+error FeeDistributor__NotFactory(address _passedAddress);
+
+/**
+* @notice Service address should be a secure P2P address, not zero.
+*/
+error FeeDistributor__ZeroAddressService();
+
+/**
+* @notice Client address should be an actual client address, not zero.
+*/
+error FeeDistributor__ZeroAddressClient();
+
+/**
+* @notice Service percent should be >= 0 and <= 100
+* @param _servicePercent passed incorrect service percent
+*/
+error FeeDistributor__InvalidServicePercent(uint256 _servicePercent);
+
+/**
+* @notice Only factory can call `initialize`.
+* @param _msgSender sender address.
+* @param _actualFactory the actual factory address that can call `initialize`.
+*/
+error FeeDistributor__NotFactoryCalled(address _msgSender, IFeeDistributorFactory _actualFactory);
+
+/**
+* @notice `initialize` should only be called once.
+* @param _existingClient address of the client with which the contact has already been initialized.
+*/
+error FeeDistributor__ClientAlreadySet(address _existingClient);
+
+/**
+* @notice Cannot call `withdraw` if the client address is not set yet.
+* @dev The client address is supposed to be set by the factory.
+*/
+error FeeDistributor__ClientNotSet();
+
+/**
+* @title Contract receiving MEV and priority fees
+* and distibuting them to the service and the client.
+*/
+contract FeeDistributor is ReentrancyGuard {
     // Type Declarations
+
     using Address for address payable;
 
     // State variables
 
-    /// address of FeeDistributorFactory
-    address private immutable i_factory;
+    /**
+    * @notice address of FeeDistributorFactory
+    */
+    IFeeDistributorFactory private immutable i_factory;
 
-    /// address of the service (P2P) fee recipient
+    /**
+    * @notice address of the service (P2P) fee recipient
+    */
     address payable private immutable i_service;
 
-    /// % of EL rewards that should go to the service (P2P)
+    /**
+    * @notice % of EL rewards that should go to the service (P2P)
+    */
     uint256 private immutable i_servicePercent;
 
-    /// % of EL rewards that should go to the client
-    uint256 private immutable i_clientPercent;
-
-    /// address of the client
+    /**
+    * @notice address of the client
+    */
     address payable private s_client;
 
     // Events
 
-    // Modifiers
-    modifier onlyFactory() {
-        if (msg.sender != i_factory) revert FeeDistributor__NotFactory();
-        _;
-    }
+    /**
+    * @notice Emits on successful withdrawal
+    * @param _serviceAmount how much wei service received
+    * @param _clientAmount how much wei client received
+    */
+    event Withdrawn(uint256 _serviceAmount, uint256 _clientAmount);
 
-    /** @dev Set values that are constant, common for all the clients, known at the initial deploy time.
+    /**
+    * @notice Emits once the client address has been set.
+    * @param _client address of the client.
+    */
+    event Initialized(address indexed _client);
+
+    /**
+    * @dev Set values that are constant, common for all the clients, known at the initial deploy time.
+    * @param _factory address of FeeDistributorFactory
+    * @param _service address of the service (P2P) fee recipient
+    * @param _servicePercent % of EL rewards that should go to the service (P2P)
     */
     constructor(
         address _factory,
         address _service,
-        uint256 _servicePercent,
-        uint256 _clientPercent
+        uint256 _servicePercent
     ) {
-        if (_servicePercent + _clientPercent != 100) {
-            revert PercentsDoNotMatch();
+        if (!ERC165Checker.supportsInterface(_factory, type(IFeeDistributorFactory).interfaceId)) {
+            revert FeeDistributor__NotFactory(_factory);
+        }
+        if (_service == address(0)) {
+            revert FeeDistributor__ZeroAddressService();
+        }
+        if (_servicePercent > 100) {
+            revert FeeDistributor__InvalidServicePercent(_servicePercent);
         }
 
-        i_factory = _factory;
+        i_factory = IFeeDistributorFactory(_factory);
         i_service = payable(_service);
         i_servicePercent = _servicePercent;
-        i_clientPercent = _clientPercent;
     }
 
     // Functions
 
-    /** @dev Set client address. Could not be in the constructor since it is different for different clients.
+    /**
+    * @notice Set client address.
+    * @dev Could not be in the constructor since it is different for different clients.
+    * @param _client the address of the client
     */
-    function initialize(address _client) external onlyFactory {
+    function initialize(address _client) external {
+        if (msg.sender != address(i_factory)) {
+            revert FeeDistributor__NotFactoryCalled(msg.sender, i_factory);
+        }
+        if (_client == address(0)) {
+            revert FeeDistributor__ZeroAddressClient();
+        }
+        if (s_client != address(0)) {
+            revert FeeDistributor__ClientAlreadySet(s_client);
+        }
+
         s_client = payable(_client);
+        emit Initialized(_client);
     }
 
-    /** @dev Withdraw the whole balance of the contract according to the pre-defined percentages.
+    /**
+    * @notice Withdraw the whole balance of the contract according to the pre-defined percentages.
     */
-    function withdraw() external {
+    function withdraw() external nonReentrant {
+        if (s_client == address(0)) {
+            revert FeeDistributor__ClientNotSet();
+        }
+
         // get the contract's balance
         uint256 balance = address(this).balance;
 
         // how much should service get
-        uint256 serviceAmount = balance * i_servicePercent / 100;
+        uint256 serviceAmount = (balance * i_servicePercent) / 100;
 
         // how much should client get
-        uint256 clientAmount = balance * i_clientPercent / 100;
+        uint256 clientAmount = balance - serviceAmount;
 
         // send ETH to service
         i_service.sendValue(serviceAmount);
 
         // send ETH to client
         s_client.sendValue(clientAmount);
+
+        emit Withdrawn(serviceAmount, clientAmount);
     }
 }

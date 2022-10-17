a64934519a6092771fe1aeaf588334074f3d102f
Alexander Biryukov <alexander.biryukov@p2p.org>
2022-10-17 10:48:54 +0300

    add role tests

--- test/factory-test.ts
+++ test/factory-test.ts
@@ -43,22 +43,6 @@ describe("FeeDistributorFactory", function () {
         factoryFactory = new FeeDistributorFactory__factory(deployerSigner)
     })
 
-    it("deployer should get DEFAULT_ADMIN_ROLE", async function () {
-        const feeDistributorFactory = await factoryFactory.deploy({gasLimit: 3000000})
-
-        const firstAdmin = await feeDistributorFactory.getRoleMember(DEFAULT_ADMIN_ROLE, 0)
-        expect(firstAdmin).to.be.equal(deployerSigner.address)
-
-        const adminCount = await feeDistributorFactory.getRoleMemberCount(DEFAULT_ADMIN_ROLE)
-        expect(adminCount).to.be.equal(1)
-
-        const hasRole = await feeDistributorFactory.hasRole(DEFAULT_ADMIN_ROLE, deployerSigner.address)
-        expect(hasRole).to.be.true
-
-        const noRole = await feeDistributorFactory.hasRole(DEFAULT_ADMIN_ROLE, nobody)
-        expect(noRole).to.be.false
-    })
-
     it("setReferenceInstance can only be called with REFERENCE_INSTANCE_SETTER_ROLE and with valid FeeDistributor", async function () {
         const feeDistributorFactory = await factoryFactory.deploy({gasLimit: 3000000})
 
@@ -138,5 +122,65 @@ describe("FeeDistributorFactory", function () {
             factorySignedByInctanceCreator,
             "FeeDistributorCreated"
         )
+    })
+
+    it("deployer should get DEFAULT_ADMIN_ROLE", async function () {
+        const feeDistributorFactory = await factoryFactory.deploy({gasLimit: 3000000})
+
+        const firstAdmin = await feeDistributorFactory.getRoleMember(DEFAULT_ADMIN_ROLE, 0)
+        expect(firstAdmin).to.be.equal(deployerSigner.address)
+
+        const adminCount = await feeDistributorFactory.getRoleMemberCount(DEFAULT_ADMIN_ROLE)
+        expect(adminCount).to.be.equal(1)
+
+        const hasRole = await feeDistributorFactory.hasRole(DEFAULT_ADMIN_ROLE, deployerSigner.address)
+        expect(hasRole).to.be.true
+
+        const noRole = await feeDistributorFactory.hasRole(DEFAULT_ADMIN_ROLE, nobody)
+        expect(noRole).to.be.false
+    })
+
+    it("no roles are assigned after deployment (except admin)", async function () {
+        const feeDistributorFactory = await factoryFactory.deploy({ gasLimit: 3000000 })
+
+        const REFERENCE_INSTANCE_SETTER_ROLECount = await feeDistributorFactory.getRoleMemberCount(REFERENCE_INSTANCE_SETTER_ROLE)
+        expect(REFERENCE_INSTANCE_SETTER_ROLECount).to.be.equal(0)
+
+        const INSTANCE_CREATOR_ROLECount = await feeDistributorFactory.getRoleMemberCount(INSTANCE_CREATOR_ROLE)
+        expect(INSTANCE_CREATOR_ROLECount).to.be.equal(0)
+
+        const ASSET_RECOVERER_ROLECount = await feeDistributorFactory.getRoleMemberCount(ASSET_RECOVERER_ROLE)
+        expect(ASSET_RECOVERER_ROLECount).to.be.equal(0)
+    })
+
+    it("cannot revoke the only admin", async function () {
+        const feeDistributorFactory = await factoryFactory.deploy({gasLimit: 3000000})
+
+        const adminCount = await feeDistributorFactory.getRoleMemberCount(DEFAULT_ADMIN_ROLE)
+        expect(adminCount).to.be.equal(1)
+
+        await expect(feeDistributorFactory.renounceRole(DEFAULT_ADMIN_ROLE, deployerSigner.address)).to.be.revertedWith(
+            `PublicTokenRecoverer__CannotRevokeTheOnlyAdmin`
+        )
+
+        await expect(feeDistributorFactory.revokeRole(DEFAULT_ADMIN_ROLE, deployerSigner.address)).to.be.revertedWith(
+            `PublicTokenRecoverer__CannotRevokeTheOnlyAdmin`
+        )
+
+        await feeDistributorFactory.grantRole(DEFAULT_ADMIN_ROLE, nobody)
+
+        const adminCountAfterAdding = await feeDistributorFactory.getRoleMemberCount(DEFAULT_ADMIN_ROLE)
+        expect(adminCountAfterAdding).to.be.equal(2)
+
+        await feeDistributorFactory.renounceRole(DEFAULT_ADMIN_ROLE, deployerSigner.address)
+
+        const adminCountAfterRemoving = await feeDistributorFactory.getRoleMemberCount(DEFAULT_ADMIN_ROLE)
+        expect(adminCountAfterRemoving).to.be.equal(1)
+
+        const deployerHasRole = await feeDistributorFactory.hasRole(DEFAULT_ADMIN_ROLE, deployerSigner.address)
+        expect(deployerHasRole).to.be.false
+
+        const newAdminHasRole = await feeDistributorFactory.hasRole(DEFAULT_ADMIN_ROLE, nobody)
+        expect(newAdminHasRole).to.be.true
     })
 })

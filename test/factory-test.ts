03d4723eb06a14d8a94a6b9626d47d6eec6d0411
Alexander Biryukov <alexander.biryukov@p2p.org>
2022-10-18 14:31:37 +0300

    test factory recovering

--- test/factory-test.ts
+++ test/factory-test.ts
@@ -4,8 +4,8 @@ import {
     FeeDistributor__factory,
     FeeDistributorFactory__factory,
     FeeDistributor,
-    FeeDistributorFactory
-} from '../typechain-types'
+    FeeDistributorFactory, MockERC20__factory, MockERC721__factory, MockERC1155__factory
+} from "../typechain-types"
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers"
 
 describe("FeeDistributorFactory", function () {
@@ -142,5 +142,62 @@ describe("FeeDistributorFactory", function () {
         await expect(feeDistributorFactory.renounceOwnership()).to.be.revertedWith(
             `Access__CannotRenounceOwnership`
         )
+    })
+
+    it("only owner can recover tokens", async function () {
+        // deoply factory
+        const feeDistributorFactory = await deployerFactoryFactory.deploy({gasLimit: 3000000})
+
+        // ERC20
+        const mockERC20Factory = new MockERC20__factory(deployerSigner)
+        const erc20Supply = ethers.utils.parseEther('100')
+        // deploy mock ERC20
+        const erc20 = await mockERC20Factory.deploy(erc20Supply)
+        // transfer mock ERC20 tokens to client instance
+        await erc20.transfer(feeDistributorFactory.address, erc20Supply)
+
+        // ERC721
+        const mockERC721Factory = new MockERC721__factory(deployerSigner)
+        // deploy mock ERC721
+        const erc721 = await mockERC721Factory.deploy()
+        // transfer mock ERC721 tokens to client instance
+        const erc721TokenId = 0
+        await erc721.transferFrom(deployerSigner.address, feeDistributorFactory.address, erc721TokenId)
+
+        // ERC1155
+        const mockERC1155Factory = new MockERC1155__factory(deployerSigner)
+        const erc1155TokenId = 0
+        const erc1155Amount = 1
+        // deploy mock ERC1155
+        const erc1155 = await mockERC1155Factory.deploy(erc1155TokenId, erc1155Amount)
+        // transfer mock ERC1155 tokens to client instance
+        // there is no unsafe transfer in ERC1155
+        await expect(erc1155.safeTransferFrom(deployerSigner.address, feeDistributorFactory.address, erc1155TokenId, erc1155Amount, "0x"))
+            .to.be.revertedWith(
+                `ERC1155: transfer to non ERC1155Receiver implementer`
+            )
+
+        const factorySignedByOwner = ownerFactoryFactory.attach(feeDistributorFactory.address)
+
+        await expect(factorySignedByOwner.transferERC20(erc20.address, nobody, erc20Supply))
+            .to.be.revertedWith(
+                `Ownable: caller is not the owner`
+            )
+
+        await expect(factorySignedByOwner.transferERC721(erc721.address, nobody, erc721TokenId, "0x"))
+            .to.be.revertedWith(
+                `Ownable: caller is not the owner`
+            )
+
+        await feeDistributorFactory.transferOwnership(owner)
+
+        await factorySignedByOwner.transferERC20(erc20.address, nobody, erc20Supply)
+        const recipientErc20Balance = await erc20.balanceOf(nobody)
+
+        await factorySignedByOwner.transferERC721(erc721.address, nobody, erc721TokenId, "0x")
+        const recipientErc721Balance = await erc721.balanceOf(nobody)
+
+        expect(recipientErc20Balance).to.be.equal(erc20Supply)
+        expect(recipientErc721Balance).to.be.equal(1)
     })
 })

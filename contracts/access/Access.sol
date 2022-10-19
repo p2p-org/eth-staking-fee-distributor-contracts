fc282690b23724ea8a49a9a8b918dccb7ec05634
Alexander Biryukov <alexander.biryukov@p2p.org>
2022-10-19 12:50:31 +0300

    rename

--- contracts/access/Access.sol
+++ contracts/access/Access.sol
@@ -28,11 +28,11 @@ error Access__CallerNotOperator(address _caller, address _operator);
 
 
 abstract contract Access is Ownable {
-    address private _operator;
+    address private s_operator;
 
     event OperatorChanges(
-        address indexed previousOperator,
-        address indexed newOperator
+        address indexed _previousOperator,
+        address indexed _newOperator
     );
 
     /**
@@ -46,16 +46,16 @@ abstract contract Access is Ownable {
     /**
      * @dev Returns the current operator.
      */
-    function operator() public view virtual returns (address) {
-        return _operator;
+    function operator() external view virtual returns (address) {
+        return s_operator;
     }
 
     /**
      * @dev Throws if the sender is not the operator.
      */
     function _checkOperator() internal view virtual {
-        if (operator() != _msgSender()) {
-            revert Access__CallerNotOperator(_msgSender(), operator());
+        if (s_operator != _msgSender()) {
+            revert Access__CallerNotOperator(_msgSender(), s_operator);
         }
     }
 
@@ -63,25 +63,25 @@ abstract contract Access is Ownable {
      * @dev Transfers operator to a new account (`newOperator`).
      * Can only be called by the current owner.
      */
-    function changeOperator(address newOperator) public virtual onlyOwner {
-        if (newOperator == address(0)) {
+    function changeOperator(address _newOperator) external virtual onlyOwner {
+        if (_newOperator == address(0)) {
             revert Access__ZeroNewOperator();
         }
-        if (newOperator == _operator) {
-            revert Access__SameOperator(newOperator);
+        if (_newOperator == s_operator) {
+            revert Access__SameOperator(_newOperator);
         }
 
-        _changeOperator(newOperator);
+        _changeOperator(_newOperator);
     }
 
     /**
      * @dev Transfers operator to a new account (`newOperator`).
      * Internal function without access restriction.
      */
-    function _changeOperator(address newOperator) internal virtual {
-        address oldOperator = _operator;
-        _operator = newOperator;
-        emit OperatorChanges(oldOperator, newOperator);
+    function _changeOperator(address _newOperator) internal virtual {
+        address oldOperator = s_operator;
+        s_operator = _newOperator;
+        emit OperatorChanges(oldOperator, _newOperator);
     }
 
     /**

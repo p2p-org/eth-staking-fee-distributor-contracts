71dd78b2320d1bdf7439c3f59820a51fdff56c03
Alexander Biryukov <alexander.biryukov@p2p.org>
2023-03-07 16:26:49 +0300

    proof for 1000000 entries

--- scripts/buildMerkleTree.ts
+++ scripts/buildMerkleTree.ts
@@ -3,15 +3,16 @@ import fs from "fs";
 import { generateMockOracleData } from "./generateMockOracleData"
 
 // (1)
-const values = generateMockOracleData(10);
-
-console.log(values);
+// replace it with data from BigQuery
+const mockOracleData = generateMockOracleData(1000000);
 
 // (2)
-const tree = StandardMerkleTree.of(values, ["bytes", "uint256"]);
+const tree = StandardMerkleTree.of(mockOracleData, ["bytes", "uint256"]);
 
 // (3)
+// Send it to the Oracle contract
 console.log('Merkle Root:', tree.root);
 
 // (4)
+// Send tree.json file to the website and to the withdrawer
 fs.writeFileSync("tree.json", JSON.stringify(tree.dump()));
--- scripts/obtainProof.ts
+++ scripts/obtainProof.ts
@@ -7,7 +7,7 @@ const tree = StandardMerkleTree.load(JSON.parse(fs.readFileSync("tree.json")));
 
 // (2)
 for (const [i, v] of tree.entries()) {
-    if (v[0] === '0x7a3ca3eb219c12175b02f0b8131c927ee67e43eb81adab3e2d77d727be4ff5533cdf0ee1300e49f34b63a96786b86a9b') {
+    if (v[0] === '0xa7a591c01356c89f6856eeb3b6226125d3b6364449d4d13dc10ff21454cbbc9e7bdf07312df2fa685e179bcfd1679050') {
         // (3)
         const proof = tree.getProof(i);
         console.log('Value:', v);

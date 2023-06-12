4f568a3e34959e5f25440c5a5d8a00391a24e7a3
Alexander Biryukov <alexander.biryukov@p2p.org>
2023-06-12 16:54:58 +0300

    deployed feedividers per pubkey

--- scripts/getIitialClientOnlyClRewards.ts
+++ scripts/getIitialClientOnlyClRewards.ts
@@ -12,23 +12,17 @@ export async function getIitialClientOnlyClRewards() {
         {oracleId: 3, firstValidatorId: 546962, validatorCount: 1},
         {oracleId: 4, firstValidatorId: 551909, validatorCount: 5},
         {oracleId: 5, firstValidatorId: 526709, validatorCount: 24},
-        {oracleId: 6, firstValidatorId: 557237, validatorCount: 21},
-        {oracleId: 7, firstValidatorId: 521543, validatorCount: 1},
+        {oracleId: 6, firstValidatorId: 521543, validatorCount: 1},
+        {oracleId: 7, firstValidatorId: 487215, validatorCount: 94},
 
-        {oracleId: 8, firstValidatorId: 487215, validatorCount: 94},
-        {oracleId: 9, firstValidatorId: 491223, validatorCount: 10},
-        {oracleId: 9, firstValidatorId: 497067, validatorCount: 4},
-        {oracleId: 9, firstValidatorId: 521576, validatorCount: 1},
-        {oracleId: 10, firstValidatorId: 491490, validatorCount: 1},
+        {oracleId: 8, firstValidatorId: 491223, validatorCount: 10},
+        {oracleId: 8, firstValidatorId: 497067, validatorCount: 4},
+        {oracleId: 8, firstValidatorId: 521576, validatorCount: 1},
+        {oracleId: 9, firstValidatorId: 491490, validatorCount: 1},
+        {oracleId: 10, firstValidatorId: 501859, validatorCount: 31},
 
-        {oracleId: 11, firstValidatorId: 485587, validatorCount: 1},
-        {oracleId: 11, firstValidatorId: 543285, validatorCount: 10},
-        {oracleId: 11, firstValidatorId: 546657, validatorCount: 22},
-        {oracleId: 11, firstValidatorId: 630081, validatorCount: 32},
-        {oracleId: 12, firstValidatorId: 501859, validatorCount: 31},
-
-        {oracleId: 13, firstValidatorId: 485640, validatorCount: 7},
-        {oracleId: 14, firstValidatorId: 564275, validatorCount: 1},
+        {oracleId: 11, firstValidatorId: 485640, validatorCount: 7},
+        {oracleId: 12, firstValidatorId: 564275, validatorCount: 1},
     ]
     const valIdsGrouped = idsAndCounts.map(rangeWithOracleIds)
     const vals = flattenWithOracleIds(valIdsGrouped)
@@ -75,28 +69,25 @@ function groupAndSumOracleIdRanges(
 
 export async function getValidatorData() {
     const idsAndCounts = [
-        {firstValidatorId: 524511, validatorCount: 1, feeDivider: '0xB2be59bed9e625A99f704030AEE6aAa5181D2e09'},
-        {firstValidatorId: 458908, validatorCount: 1, feeDivider: '0xDBEF4D23833a3a6e91D620caa0FE72F594D2E153'},
-        {firstValidatorId: 483996, validatorCount: 1, feeDivider: '0x2A95C85a85767bE7E91Ff10f1A255F31c460811c'},
-        {firstValidatorId: 551909, validatorCount: 5, feeDivider: '0xac1821abb3dd4D890FC8FB960d796469A82D2E05'},
-        {firstValidatorId: 526709, validatorCount: 24, feeDivider: '0xBa63343A228B6F8C35f1013B5F2527162BAEeE3E'},
+        {oracleId: 1, firstValidatorId: 524511, validatorCount: 1, feeDivider: '0x05660a51A688E9F102f0bF46Cb6e64efc3381408'},
+        {oracleId: 1, firstValidatorId: 458908, validatorCount: 1, feeDivider: '0x05660a51A688E9F102f0bF46Cb6e64efc3381408'},
+        {oracleId: 2, firstValidatorId: 483996, validatorCount: 1, feeDivider: '0xB21E436D4deA46adbB3F5276d357180e694dC8b7'},
+        {oracleId: 2, firstValidatorId: 486009, validatorCount: 37, feeDivider: '0xB21E436D4deA46adbB3F5276d357180e694dC8b7'},
+        {oracleId: 2, firstValidatorId: 486382, validatorCount: 40, feeDivider: '0xB21E436D4deA46adbB3F5276d357180e694dC8b7'},
 
-        {firstValidatorId: 565823, validatorCount: 10, feeDivider: '0xfD80D04e8FdCFE9dc1707D4f9D4855E706bc1A11'},
-        {firstValidatorId: 557237, validatorCount: 21, feeDivider: '0xE22B75c7d7238C6D66d14953d2b4211a10ef3635'},
-        {firstValidatorId: 670938, validatorCount: 10, feeDivider: '0x5933dD898E74491c30Efe0855BD0fB505d577A6A'},
-        {firstValidatorId: 521543, validatorCount: 1, feeDivider: '0x053Ab0F12e02b819a3E3E903489a711f62c512F2'},
-        {firstValidatorId: 487215, validatorCount: 94, feeDivider: '0x8f9C7e2373D7410Ef2F49480dd27b1b2aA43439B'},
+        {oracleId: 4, firstValidatorId: 551909, validatorCount: 5, feeDivider: '0x0216CfF879B17B5beDe762B3e4071B49c8696a25'},
+        {oracleId: 5, firstValidatorId: 526709, validatorCount: 24, feeDivider: '0x3d1be477186D64B4f4fba7cB773216dA0C2B474F'},
+        {oracleId: 6, firstValidatorId: 521543, validatorCount: 1, feeDivider: '0xBB52D5B7F2AE4BBe0967F4626d07aba89F462Ff7'},
+        {oracleId: 7, firstValidatorId: 487215, validatorCount: 94, feeDivider: '0x36c0A9B9D3799bddbcED6f584a70060DF368a073'},
 
-        {firstValidatorId: 521576, validatorCount: 1, feeDivider: '0x0723A225FddB1328a6B0B6D345F04F7bed06a994'},
-        {firstValidatorId: 497067, validatorCount: 4, feeDivider: '0x5ae668dB49Bf848AE2c10d4e3f863B5E46e79F68'},
-        {firstValidatorId: 491223, validatorCount: 10, feeDivider: '0x91F177e861B1ef3b1321224d93F27Fc479186980'},
-        {firstValidatorId: 564275, validatorCount: 1, feeDivider: '0xfBe1a357586e04Fd5B315b03CC599e3CCb4729aE'},
-        {firstValidatorId: 491490, validatorCount: 1, feeDivider: '0xa543F5A85AfCcb3C3789026900E9a8C61Da66851'},
+        {oracleId: 8, firstValidatorId: 491223, validatorCount: 10, feeDivider: '0xF555a33C07eEAB91EE4bF1Bc950c14Fc8a9EF1aB'},
+        {oracleId: 8, firstValidatorId: 497067, validatorCount: 4, feeDivider: '0xF555a33C07eEAB91EE4bF1Bc950c14Fc8a9EF1aB'},
+        {oracleId: 8, firstValidatorId: 521576, validatorCount: 1, feeDivider: '0xF555a33C07eEAB91EE4bF1Bc950c14Fc8a9EF1aB'},
+        {oracleId: 9, firstValidatorId: 491490, validatorCount: 1, feeDivider: '0xe39640588571F507298F688bf8b1F68D9600F96c'},
+        {oracleId: 10, firstValidatorId: 501859, validatorCount: 31, feeDivider: '0xd6781E7F15752d3F5764609a42A803A7345DE25c'},
 
-        {firstValidatorId: 546657, validatorCount: 22, feeDivider: '0x8B71bC670b80b456c5B183a84b45Ea2D84BeD8C1'},
-        {firstValidatorId: 543285, validatorCount: 10, feeDivider: '0xA564beF728805cEab727a63134526b40edc4e81F'},
-        {firstValidatorId: 501859, validatorCount: 31, feeDivider: '0xF95306839BEE5b73004F501F57db16F477537618'},
-        {firstValidatorId: 485640, validatorCount: 7, feeDivider: '0xf88Fc7298a31FC240c0eC8C9671766142d398Eaf'},
+        {oracleId: 11, firstValidatorId: 485640, validatorCount: 7, feeDivider: '0xD3825cd251eF9AD7b9Bffec5851e438e15D0a358'},
+        {oracleId: 12, firstValidatorId: 564275, validatorCount: 1, feeDivider: '0x72288Ee8c6304778D5d78AaB71F50acAC61E7B81'},
     ]
     const valIdsGrouped = idsAndCounts.map(rangeWithFeeDividers)
     const vals = flattenWithFeeDividers(valIdsGrouped)
@@ -146,13 +137,14 @@ export function flattenWithOracleIds(array: { index: number; oracleId: number }[
 }
 
 async function main() {
+    console.log("pubkey,feedivider")
     const data = await getValidatorData();
     data.map(d => console.log(d.pubKey + ',' + d.feeDivider))
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
-// main().catch((error) => {
-//     console.error(error);
-//     process.exitCode = 1;
-// });
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
